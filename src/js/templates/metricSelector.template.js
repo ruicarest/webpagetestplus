bf198074663dd552036d9b8605b38d3de9a1f0fc
Tiago Araujo <tiago.araujo@farfetch.com>
2018-12-10 18:56:47 +0000

    feat: New layout to export page

--- src/js/templates/metricSelector.template.js
+++ src/js/templates/metricSelector.template.js
@@ -1,5 +1,5 @@
 Template.add('metricSelector', Template.create`
-<div class="metric">
+<div>
     <span uk-icon="icon: table; ratio: 0.5"></span>
     <label>
         <input class="uk-checkbox" type="checkbox" name="metrics" id="checkbox_${'name'}" value="${'name'}" ${'checked ? "checked" : ""'}> ${'description'}
